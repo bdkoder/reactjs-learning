6371d106955b21946960ceb497c7709760776324
Shahidul Islam <bdkoder@gmail.com>
2023-09-23 00:09:03 +0600

    Learning Evaluation Expression

--- tea-and-code/01basics/src/App.jsx
+++ tea-and-code/01basics/src/App.jsx
@@ -4,11 +4,12 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const userName = 'John Doe';
 
   return (
     <>
       <h1>Learning ReactJS</h1>
+      <h3>Username:- {userName}</h3>
     </>
   )
 }
