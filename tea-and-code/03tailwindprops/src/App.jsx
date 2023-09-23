eb5169c4a69622e97dfbcffa278b4a5e6e3837c1
Shahidul Islam <bdkoder@gmail.com>
2023-09-23 11:47:57 +0600

    Handiling Props

--- tea-and-code/03tailwindprops/src/App.jsx
+++ tea-and-code/03tailwindprops/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind Test</h1>
-      <Card />
+      <Card userName='Shahidul Islam' btnText='View Profile'/>
     </>
   )
 }
