e098ffacef78df5da17a03237f2c71a7e5a52742
Shahidul Islam <bdkoder@gmail.com>
2024-06-18 14:31:57 +0600

    Working on React Router

---
+++ tea-and-code/07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Github() {
+    return (
+        <div>
+            <h1 className="text-2xl font-medium text-center py-10">Github</h1>
+            <div className="grid place-items-center">
+                <img className="w-96" src="https://i.ibb.co/2M7rtLk/Remote1.png" alt="image2" />
+            </div>
+        </div>
+    );
+}
