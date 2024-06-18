0736ce78d8d31e9729ae1763c71782b6cb73951c
Shahidul Islam <bdkoder@gmail.com>
2024-06-18 14:46:06 +0600

    Github Profile added

--- tea-and-code/07reactRouter/src/components/Github/Github.jsx
+++ tea-and-code/07reactRouter/src/components/Github/Github.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
 
 export default function Github() {
+    const [data, setData] = useState([])
+
+
+useEffect(() => {
+    fetch('https://api.github.com/users/bdkoder')
+        .then((response) => response.json())
+        .then((data) => setData(data));
+
+}
+, []);
     return (
         <div>
-            <h1 className="text-2xl font-medium text-center py-10">Github</h1>
+            <h1 className="text-2xl font-medium text-center py-2">Github</h1>
             <div className="grid place-items-center">
-                <img className="w-96" src="https://i.ibb.co/2M7rtLk/Remote1.png" alt="image2" />
+                <h2 className="text-2xl font-medium text-center py-2">{data.name}</h2>
+            </div>
+            <div className="grid place-items-center">
+                <h2 className="text-2xl font-medium text-center py-2">{data.bio}</h2>
+            </div>
+            <div className="grid place-items-center py-2">
+                <img className="w-96" src={data.avatar_url} alt="image2" />
             </div>
         </div>
     );
